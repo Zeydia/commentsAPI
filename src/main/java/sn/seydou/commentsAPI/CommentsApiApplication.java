067a9e4418bd5ba10bf59237a40e054f69d9adaf
Zeydia <douseydiallo2002@gmail.com>
2025-01-14 17:29:42 +0000

    lancement d'un mapping de test

---
+++ src/main/java/sn/seydou/commentsAPI/CommentsApiApplication.java
@@ -0,0 +1,13 @@
+package sn.seydou.commentsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/seydou/commentsAPI/CommentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.seydou.commentsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
